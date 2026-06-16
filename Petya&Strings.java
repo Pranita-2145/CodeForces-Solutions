08ec9070a34930af6f5679167fd4704a1184b10c
pranita-2145 <bpranita2005@gmail.com>
2026-06-16 19:37:00 +0530

    Create Petya&Strings.java

---
+++ Petya&Strings.java
@@ -0,0 +1,30 @@
+//Que 112A
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String s1=sc.nextLine();
+        String s2=sc.nextLine();
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
+
+        for(int i=0; i<s1.length(); i++){
+            if(s1.charAt(i)> s2.charAt(i)){
+                System.out.println(1);
+                return;
+            }
+            if(s1.charAt(i)< s2.charAt(i)){
+                System.out.println(-1);
+                return;
+            }
+        }
+        System.out.println(0);
+        sc.close();
+       
+
+
+    }
+}
