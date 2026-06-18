a90e27ac65ac88ea9157e7de26d754cf2870af6e
pranita-2145 <bpranita2005@gmail.com>
2026-06-18 19:16:35 +0530

    Create Helpflmaths.java

---
+++ Helpflmaths.java
@@ -0,0 +1,33 @@
+//339A - Helpful Maths
+
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        String[] arr = s.split("\\+");
+
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            sb.append(arr[i]);
+
+            if (i != arr.length - 1) {
+                sb.append("+");
+            }
+        }
+
+        System.out.println(sb);
+
+        sc.close();
+    }
+}
